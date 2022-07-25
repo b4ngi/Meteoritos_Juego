8037f7e3670fa3be9e9e713b04b736f3f629622b
b4ngi <ignaciobenetucci@gmail.com>
2022-07-25 18:52:03 -0300

    Escudo
    - Activar y desactivar escudo
    - SFX escudo
    - Animaciones del escudo

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -23,6 +23,7 @@ onready var estela: Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx: Motor = $MotorSFX
 onready var impacto_sfx: AudioStreamPlayer2D = $ImpactoSFX
 onready var colisionador: CollisionShape2D = $CollisionShape2D
+onready var escudo: Escudo = $Escudo
 
 # Metodos
 func _ready() -> void:
@@ -56,6 +57,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if (event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
 		motor_sfx.sonido_off()
+	
+	# Control Escudo
+	if event.is_action_pressed("escudo") and not escudo.get_esta_activado():
+		escudo.activar()
 
 func destruir() -> void:
 	controlador_estados(ESTADO.MUERTO)
