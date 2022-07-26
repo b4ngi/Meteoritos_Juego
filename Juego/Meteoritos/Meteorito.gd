609876698df0abbb8381c99e7d42923e66247096
b4ngi <ignaciobenetucci@gmail.com>
2022-07-26 19:17:12 -0300

    Sector meteoritos
    - Sector de meteoritos
        - Spawn, camara, etc..
    - Zona de peligro
    - Transicion de camaras
    - Animaciones

--- Juego/Meteoritos/Meteorito.gd
+++ Juego/Meteoritos/Meteorito.gd
@@ -9,13 +9,21 @@ export var hitpoints_base: float = 10.0
 
 ## Atributos
 var hitpoints: float
+var esta_en_sector: bool = true setget set_esta_en_sector
+var pos_spawn_original: Vector2
+var vel_spawn_original: Vector2
 
 onready var impacto_sfx: AudioStreamPlayer2D = $ImpactoSFX
 onready var animacion_impacto: AnimationPlayer = $AnimationPlayer
 
+# Seters y Getters
+func set_esta_en_sector(valor: bool) -> void:
+	esta_en_sector = valor
+
 # Constructor
 func crear(pos: Vector2, dir: Vector2, tamanio: float) -> void:
 	position = pos
+	pos_spawn_original = position
 	# Calcular masa, tamanio de sprite y de colisionador
 	mass *= tamanio
 	$Sprite.scale = Vector2.ONE * tamanio
@@ -27,12 +35,23 @@ func crear(pos: Vector2, dir: Vector2, tamanio: float) -> void:
 	# Calcular velocidades
 	linear_velocity = (vel_lineal_base * dir / tamanio) * aleatorizar_velocidad()
 	angular_velocity = (vel_ang_base / tamanio) * aleatorizar_velocidad()
+	vel_spawn_original = linear_velocity
 	# Calcular hitpoints
 	hitpoints = hitpoints_base * tamanio
 
 # Metodos
 func _ready() -> void:
 	angular_velocity = vel_ang_base
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if esta_en_sector:
+		return
+	
+	var mi_transform := state.get_transform()
+	mi_transform.origin = pos_spawn_original
+	linear_velocity = vel_spawn_original
+	state.set_transform(mi_transform)
+	esta_en_sector = true
 
 # Metodos custom
 func recibir_danio(danio: float) -> void:
---
+++ Juego/Peligros/SectorDePeligro.gd
@@ -0,0 +1,24 @@
+# SectorDePeligro.gd
+class_name SectorDePeligro
+extends Area2D
+
+export(String, "vacio", "Meteorite", "Enemy") var tipo_peligro
+export var numero_peligros: int = 10
+
+# Seniales
+func _on_body_entered(_body: Node) -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.1), "timeout")
+	enviar_senial()
+
+func enviar_senial() -> void:
+	Eventos.emit_signal(
+		"nave_en_sector_peligro",
+		$PosicionCentroSector.global_position,
+		tipo_peligro,
+		numero_peligros
+	)
+	queue_free()
+
+func _ready():
+	pass
