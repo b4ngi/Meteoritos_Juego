5b4de7068bb404d642265476623e33b7aa22b752
b4ngi <ignaciobenetucci@gmail.com>
2022-07-28 02:11:59 -0300

    Estacion de recarga
    - Escena de estacion de recarga
         - Sprite, animacion, etc.
    - Recarga de escudo y laser
    - Energia maxima de escudo y laser
    - Energia de la estacion
    - SFX de carga y sin energia

--- Juego/Naves/Player/Escudo/Escudo.gd
+++ Juego/Naves/Player/Escudo/Escudo.gd
@@ -3,6 +3,7 @@ class_name Escudo
 extends Area2D
 
 var esta_activado: bool = false setget ,get_esta_activado
+var energia_original: float
 
 export var energia: float = 8.0
 export var radio_desgaste: float = -1.6
@@ -13,16 +14,24 @@ func get_esta_activado() -> bool:
 
 # Metodos
 func _ready() -> void:
+	energia_original = energia
 	set_process(false)
 	controlar_colisionador(true)
 
 func _process(delta: float) -> void:
-	energia += radio_desgaste * delta
+	controlar_energia(radio_desgaste * delta)
 	
 	if energia <= 0.0:
 		desactivar()
 
 # Metodos custom
+func controlar_energia(consumo: float) -> void:
+	energia += consumo
+	if energia > energia_original:
+		energia = energia_original
+	elif energia <= 0.0:
+		desactivar()
+
 func controlar_colisionador(esta_desactivado: bool) -> void:
 	$CollisionShape2D.set_deferred("disabled", esta_desactivado)
 
