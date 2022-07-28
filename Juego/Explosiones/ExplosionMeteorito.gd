fe0b2f50746dd3b9d24c1626fb28420958ada9cb
b4ngi <ignaciobenetucci@gmail.com>
2022-07-28 03:05:48 -0300

    Pulido
    - Correccion error de fondo
    - Pulido en camara al explotar

--- Juego/Explosiones/ExplosionMeteorito.gd
+++ Juego/Explosiones/ExplosionMeteorito.gd
@@ -2,8 +2,18 @@
 class_name ExplosionMeteorito
 extends Node2D
 
+#func _ready() -> void:
+#	$AnimationPlayer.play(elegir_explosion_aleatoria())
 
+#func elegir_explosion_aleatoria() -> String:
+#	randomize()
+#	var num_anim: int = $AnimationPlayer.get_animacion_list().size() - 1
+#	var indice_anim_aleatoria: int = randi() % num_anim + 1
+#	var lista_animacion: Array = $AnimationPlayer.get_animation_list()
+#	
+#	return lista_animacion[indice_anim_aleatoria]
 
+## Si se agregan mas animaciones para las explosiones de los meteoritos
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	queue_free()
