f2fd89d81b02f6af3f2bf4dd96330d6fbbbddc6a
b4ngi <ignaciobenetucci@gmail.com>
2022-08-04 03:43:49 -0300

    IA Orbital
    - Ruta orbitales
    - Spawn de distintos orbitales
    - Velocidad, distancia de disparo, etc.

--- Juego/Estaciones/BaseEnemiga/BaseEnemiga.gd
+++ Juego/Estaciones/BaseEnemiga/BaseEnemiga.gd
@@ -4,13 +4,19 @@ extends Node2D
 
 export var hitpoints: float = 30.0
 export var orbital: PackedScene = null
+export var numero_orbitales: int = 9
+export var intervalo_spawn: float = 0.8
 
 onready var impacto_sfx: AudioStreamPlayer2D = $ImpactoSFX
+onready var timer_spawner: Timer = $TimerSpawnEnemigos
 
+## Atributos
 var esta_destruida: bool = false
+var posicion_spawn: Vector2 = Vector2.ZERO
 
 ## Metodos
 func _ready() -> void:
+	timer_spawner.wait_time = intervalo_spawn
 	$AnimationPlayer.play(elegir_animacion_aleatoria())
 
 ## Metodos custom
@@ -41,11 +47,14 @@ func destruir() -> void:
 	queue_free()
 
 func spawnear_orbital() -> void:
-	var pos_spawn: Vector2 = deteccion_cuadrante()
+	numero_orbitales -= 1
+	$RutaEnemigo.global_position = global_position
+	
 	var new_orbital: EnemigoOrbital = orbital.instance()
 	new_orbital.crear(
-		global_position + pos_spawn,
-		self
+		global_position + posicion_spawn,
+		self,
+		$RutaEnemigo
 	)
 	Eventos.emit_signal("spawn_orbital", new_orbital)
 
@@ -60,16 +69,20 @@ func deteccion_cuadrante() -> Vector2:
 	
 	if abs(angulo_player) <= 45.0:
 		# Player entra por la derecha
+		$RutaEnemigo.rotation_degrees = 180.0
 		return $PosicionesSpawn/Este.position
 	elif abs(angulo_player) > 135.0 and abs(angulo_player) <= 180.0:
 		# Player entra por izquierda
+		$RutaEnemigo.rotation_degrees = 0.0
 		return $PosicionesSpawn/Oeste.position
 	elif abs(angulo_player) > 45.0 and abs(angulo_player) <= 135.0:
 		# Player entra por arriba o por abajo
 		if sign(angulo_player) > 0:
 			# Player entra por abajo
+			$RutaEnemigo.rotation_degrees = 270.0
 			return $PosicionesSpawn/Sur.position
 		else:
+			$RutaEnemigo.rotation_degrees = 90.0
 			return $PosicionesSpawn/Norte.position
 	
 	return $PosicionesSpawn/Norte.position
@@ -81,4 +94,12 @@ func _on_AreaColision_body_entered(body: Node) -> void:
 
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	$VisibilityNotifier2D.queue_free()
+	posicion_spawn = deteccion_cuadrante()
+	spawnear_orbital()
+	timer_spawner.start()
+
+func _on_TimerSpawnEnemigos_timeout() -> void:
+	if numero_orbitales == 0:
+		timer_spawner.stop()
+		return
 	spawnear_orbital()
--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -121,17 +121,11 @@ func _on_nave_destruida(nave: Player, posicion: Vector2, num_explosiones: int) -
 	if nave is Player:
 		transicion_camaras(
 			posicion,
-			posicion + crear_posicion_aleatoria(-200.0, 200.0),
+			posicion + crear_posicion_aleatoria(200.0, 200.0),
 			camara_nivel,
 			tiempo_transicion_camara
 		)
-	crear_explosion(posicion, num_explosiones, 0.6, Vector2(100.0, 50.0))
-	
-	for i in range(num_explosiones):
-		var new_explosion: Node2D = explosion.instance()
-		new_explosion.global_position = posicion + crear_posicion_aleatoria(100.0, 50.0)
-		add_child(new_explosion)
-		yield(get_tree().create_timer(0.6), "timeout")
+	crear_explosion(posicion, 1.0, 0.6, Vector2(100.0, 50.0))
 
 func _on_spawn_meteoritos(pos_spawn: Vector2, dir_meteorito: Vector2, tamanio: float) -> void:
 	var new_meteorito: Meteorito = meteorito.instance()
