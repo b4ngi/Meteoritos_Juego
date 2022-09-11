90f68d2a6126da16d959db120646635501a0ffe7
b4ngi <ignaciobenetucci@gmail.com>
2022-09-10 23:41:13 -0300

    Retoque de niveles y correcion de bug
    - Retoque en los niveles.
    - Correcion de un bug que existia a la hora de eliminar el icono de la estacion de recarga sin energia del minimapa. Cuando se instanciaba otro icono en el minimapa, el icono de la estacion volvía a aparecer.

--- Juego/Estaciones/EstacionRecarga/EstacionRecarga.gd
+++ Juego/Estaciones/EstacionRecarga/EstacionRecarga.gd
@@ -41,6 +41,7 @@ func controlar_energia() -> void:
 	if energia <= 0.0:
 		Eventos.emit_signal("ocultar_energia_laser")
 		Eventos.emit_signal("ocultar_energia_escudo")
+		remove_from_group("minimapa")
 		Eventos.emit_signal("minimapa_objeto_destruido", self)
 		$VacioSFX.play()
 		barra_energia.visible = false
